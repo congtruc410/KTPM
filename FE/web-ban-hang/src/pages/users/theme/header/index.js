f5fcff8b81ba544cc84b46905e8d44b51bc77e3d
Công Trưc <1851010147truc@ou.edu.vn>
2024-03-19 20:33:14 +0700

    Lam phan menu

--- FE/web-ban-hang/src/pages/users/theme/header/index.js
+++ FE/web-ban-hang/src/pages/users/theme/header/index.js
@@ -1,7 +1,142 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+import "./style.scss";
+
+import {
+  AiOutlineFacebook,
+  AiOutlineGlobal,
+  AiOutlineUser,
+  AiOutlineMail,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+import { CiInstagram, CiLinkedin } from "react-icons/ci";
+import { Link } from "react-router-dom";
+import { formatter } from "utils/fomater";
+import { ROUTERS } from "utils/router";
 
 const Header = () => {
-  return <h1>Header</h1>;
+  const [menus, setMenus] = useState([
+    {
+      name: "Trang chủ",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Cửa hàng",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Sản phẩm",
+      path: "",
+      isShowSubmenu: false,
+      child: [
+        {
+          name: "Thịt",
+          path: "",
+        },
+        {
+          name: "Rau củ",
+          path: "",
+        },
+        {
+          name: "Thức ăn nhanh",
+          path: "",
+        },
+      ],
+    },
+    {
+      name: "Bài viết",
+      path: "",
+    },
+    {
+      name: "Liên hệ",
+      path: "",
+    },
+  ]);
+
+  return (
+    <>
+      <div className="header__top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 header__top-left">
+              <ul>
+                <li className="">
+                  <AiOutlineMail />
+                  Hello@gmail.com
+                </li>
+                <li className="">Free ship more {formatter(20000)}</li>
+              </ul>
+            </div>
+            <div className="col-6 header__top-right">
+              <ul>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineFacebook />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <CiInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <CiLinkedin />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineGlobal />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineUser />
+                  </Link>
+                  <span>Đăng nhập</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-xl-3">
+            <div className="header__logo">
+              <h1>SiVi SHOP</h1>
+            </div>
+          </div>
+          <div className="col-lg-3 col-xl-6">
+            <div className="header__menu">
+              <ul>
+                {menus?.map((menu, menuKey) => (
+                  <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                    <Link to={menu.path}>{menu?.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-3 col-xl-3">
+            <div className="header__cart">
+              <div className="header__cart-price">
+                <span>{formatter(1001230)}</span>
+              </div>
+              <ul>
+                <li>
+                  <Link to="#!">
+                    <AiOutlineShoppingCart />
+                    <span>5</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default memo(Header);
